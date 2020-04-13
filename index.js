11e18be57ab9a54252cb47dd5c471d8f15b6f1be
RikBala <rik280491@googlemail.com>
2020-04-13 18:58:38 +0100

    Index and show pages. Basic functionality and filter is working

---
+++ index.js
@@ -0,0 +1,75 @@
+cocktailsAPI = "http://localhost:3000/cocktails";
+const cardList = document.querySelector(".cards-list");
+
+const ini = () => {
+	fetch(cocktailsAPI)
+		.then((resp) => resp.json())
+		.then((cocktails) => renderCocktails(cocktails));
+};
+
+const renderCocktails = (cocktails) => {
+	cocktails.forEach((cocktail) => renderCocktail(cocktail));
+};
+
+const renderCocktail = (cocktail) => {
+    
+    const card = document.createElement("div");
+	card.className = "card";
+
+	const cardImageDiv = document.createElement("div");
+	cardImageDiv.className = "card_image";
+	const cardImage = document.createElement("img");
+	cardImage.src = cocktail.img_url;
+	cardImageDiv.append(cardImage);
+
+	const cardTitleDiv = document.createElement("div");
+	cardTitleDiv.className = "card_title title-white";
+	const cardTitle = document.createElement("h5");
+	cardTitle.innerText = cocktail.name;
+	cardTitleDiv.append(cardTitle);
+
+	card.addEventListener("click", () => {
+		cardList.innerHTML = ""
+		renderShowPage(cocktail);
+	});
+
+	card.append(cardImageDiv, cardTitleDiv);
+	cardList.append(card);
+};
+
+const filterCocktails = () => {
+    
+    fetch(cocktailsAPI)
+		.then((resp) => resp.json())
+		.then((cocktails) => {
+            const cocktailNameArray = cocktails.map((cocktail) => cocktail.name);
+
+			const cocktailFilter = document.querySelector("#cocktail-filter");
+
+            
+			cocktailFilter.options[cocktailFilter.options.length] = new Option("All");
+			for (index in cocktailNameArray) {
+				cocktailFilter.options[cocktailFilter.options.length] = new Option(
+					cocktailNameArray[index]
+				);
+			}
+
+			cocktailFilter.addEventListener("change", () => {
+                cardList.innerHTML = ""
+				const filterValue = document.querySelector("#cocktail-filter").value;
+                
+				if (filterValue === "All") {
+					cocktails.forEach((cocktail) => renderCocktail(cocktail));
+				} else {
+					const targetCocktail = cocktails.filter(
+						(cocktail) => cocktail.name === filterValue
+					);
+                    renderCocktail(targetCocktail[0]);
+                    
+				}
+			});
+		});
+};
+
+ini();
+filterCocktails();
---
+++ show.js
@@ -0,0 +1,29 @@
+
+
+const body = document.querySelector("body")
+
+const renderShowPage = (cocktail) => {
+    fetch(`${cocktailsAPI}/${cocktail.id}`).then(resp => resp.json())
+    .then(() => cocktailShowPage(cocktail))
+}
+
+const cocktailShowPage = cocktail => {
+    const allCocktailsButton = document.createElement("button")
+    allCocktailsButton.innerText = "All Cocktails"
+
+    allCocktailsButton.addEventListener("click", () => {
+        ini();
+    })
+    const cocktailName = document.createElement("h1")
+    cocktailName.innerText = cocktail.name
+
+    const cocktailImg = document.createElement("img")
+    cocktailImg.src = cocktail.img_url 
+    cocktailImg.height = "500"
+
+    const cocktailMethod = document.createElement("p")
+    cocktailMethod.innerText = cocktail.method 
+
+    
+    body.append(allCocktailsButton, cocktailName, cocktailImg, cocktailMethod)
+}
